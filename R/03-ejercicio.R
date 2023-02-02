3cc7df24f75598af440549baba06f9c192e1bd76
Pablo Sicilia Andrade <psicilia@lcg.unam.mx>
2023-02-02 01:22:47 -0600

    Agregamos a los autores, el resumen y los datos extra

--- R/03-ejercicio.R
+++ R/03-ejercicio.R
@@ -1,9 +1,13 @@
 # Nombre: crisprViz
 #
-# Autores:
+# Autores:Jean-Philippe Fortin [aut, cre], Luke Hoberecht [aut]
 #
-# Enlace:
+# Enlace:https://github.com/crisprVerse/crisprViz
 #
-# Resumen:
+# Resumen:Provides functionalities to visualize and contextualize
+# CRISPR guide RNAs (gRNAs) on genomic tracks across nucleases
+# and applications. Works in conjunction with the crisprBase and
+# crisprDesign Bioconductor packages. Plots are produced using
+# the Gviz framework.
 #
-# datos extra:
+# datos extra: aun no logra pasar las pruebas de building en nigun SO
